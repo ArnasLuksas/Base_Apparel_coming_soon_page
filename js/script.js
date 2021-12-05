748c081b0b9c1477f1e9a9a1c2b679df355fd0eb
Arnas <arnasluksas@gmail.com>
2021-12-05 09:53:42 +0200

    checking input is empty or not and checking email includes @ to input (works not how I want..)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 const changeImg = document.querySelector('.changeImg');
+const error = document.querySelector('.error');
+const input = document.querySelector('input');
+const span = document.querySelector('.error span');
+const form = document.querySelector('form')
 
 const getSize = onresize = () => {
     if (innerWidth >= 1440) {
@@ -7,5 +11,32 @@ const getSize = onresize = () => {
         changeImg.setAttribute("src", "images/hero-mobile.jpg");
     }
 }
-
     getSize();
+
+const validateForm = addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const inputVal = input.value.trim()
+
+    if(inputVal.includes('@')){
+        error.style.display = 'none';
+        form.innerHTML += `
+        <div class="working">
+           <span>Working :) your email is: ${inputVal}</span>
+        </div>
+        <button type="submit" value="Submit" ><img src="images/icon-arrow.svg" alt=""></button>
+      `; 
+
+    }else if(inputVal === '' || !inputVal.includes('@') ){
+
+        error.style.display = 'block';
+
+     
+        
+    }
+
+});
+
+
+
+
+
