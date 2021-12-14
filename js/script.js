aee328da95810fe3edecccd4fdf2b7c109fec6ff
Arnas <arnasluksas@gmail.com>
2021-12-14 15:17:04 +0200

    changed indcudes() to regex test(), added arial-label='Send mail' for button, changed input type from submit to email

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ const span = document.querySelector('.error span');
 const form = document.querySelector('form')
 
 const getSize = onresize = () => {
-    if (innerWidth >= 1439) {
+    if (innerWidth >= 1440) {
         changeImg.setAttribute("src", "images/hero-desktop.jpg");
-    } else if (innerWidth < 1439) {
+    } else if (innerWidth < 1440) {
         changeImg.setAttribute("src", "images/hero-mobile.jpg");
     }
 }
@@ -15,15 +15,12 @@ getSize();
 
 const validateForm = addEventListener('submit', (e) => {
     e.preventDefault()
-    const inputVal = input.value.trim()
-    if (!inputVal.includes('@') || inputVal === '') {
-        error.style.display = 'block';
-    } else if (inputVal.includes('@') || !inputVal === '') {
+    const inputVal = input.value.trim();
+    const re = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+\.[a-zA-Z]+$/;
+    const testing = re.test(inputVal);
+    if(!testing){
+         error.style.display = 'block';
+    }else{
         error.style.display = 'none';
     }
 });
-
-
-
-
-
